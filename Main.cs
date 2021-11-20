d9e686eba504001561bb06fd3f9d3bd95d79c6aa
DestroyedClone <destroyedclone@gmail.com>
2021-11-19 18:02:16 -0800

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using R2API.Utils;
+using RoR2;
+using System.Security;
+using System.Security.Permissions;
+
+[module: UnverifiableCode]
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
+
+namespace FMPModelSwap
+{
+    [BepInPlugin("com.DestroyedClone.CollisionLODOverride", "Collision LOD Override", "1.0.0")]
+    [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
+    [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
+    public class Main : BaseUnityPlugin
+    {
+    }
+}
